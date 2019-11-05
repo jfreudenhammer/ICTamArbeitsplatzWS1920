f3f3020979b13b151a7b4ff5303625258a97661f
jfreudenhammer <julian.freudenhammer@rwth-aachen.de>
2019-11-05 10:19:30 +0100

    AnalysisSkript angelegt

---
+++ AnalysisScript.R
@@ -0,0 +1,10 @@
+# AnalysisScript.R
+## Teamname: ICT am Arbeitsplatz
+
+# Pakete laden: ----
+
+# Rohdaten laden: ----
+
+# DataCleaning und Skalenberechnung: ----
+
+# Statistische Analyse und Grafiken: ----
